775ad2dad49559b0f31dc17ecc32c0238fb2cf3f
mito <78819172+mitoiscool@users.noreply.github.com>
2024-06-07 10:55:15 -0400

    Update .gitignore, create tests

---
+++ FoldingChair.Tests.NetCore.Arithmetic/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(Add(10, 12));
+Console.WriteLine(Sub(10, 12));
+Console.WriteLine(Mul(10, 12));
+Console.WriteLine(Div(10, 12));
+
+static int Add(int i1, int i2)
+{
+    return i1 + i2;
+}
+        
+static int Sub(int i1, int i2)
+{
+    return i1 - i2;
+}
+        
+static int Mul(int i1, int i2)
+{
+    return i1 * i2;
+}
+        
+static int Div(int i1, int i2)
+{
+    return i1 / i2;
+}
